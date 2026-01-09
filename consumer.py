e96ccc4ea071100367834b8c9614266b8b90736d
Arunvel Arunachalam <aryan.arunachalam@gmail.com>
2026-01-09 07:05:08 +0000

    Update consumer.py

--- consumer.py
+++ consumer.py
@@ -1,6 +1,7 @@
 from kafka import KafkaConsumer
 import json
 import sqlite3
+from datetime import datetime
 
 # -----------------------------
 # Database Setup
@@ -10,7 +11,6 @@ cur = conn.cursor()
 
 cur.execute("""
 CREATE TABLE IF NOT EXISTS vulnerabilities (
-    pipeline_id TEXT,
     timestamp TEXT,
     image TEXT,
     severity TEXT,
@@ -44,19 +44,17 @@ print("Kafka consumer started...")
 # -----------------------------
 for msg in consumer:
     report = msg.value
-    pipeline_id = report.get("pipeline_id", "unknown")
-    ts = report.get("timestamp")  # use timestamp from JSON
+    ts = datetime.utcnow().isoformat()  # timestamp when consumed
 
-    for result in report.get("vulnerabilities", []):
+    for result in report.get("Results", []):
         image = result.get("Target", "unknown")
 
         for vuln in result.get("Vulnerabilities", []):
             cur.execute("""
                 INSERT INTO vulnerabilities
-                (pipeline_id, timestamp, image, severity, vuln_id, package, fixed_version, title)
-                VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+                (timestamp, image, severity, vuln_id, package, fixed_version, title)
+                VALUES (?, ?, ?, ?, ?, ?, ?)
             """, (
-                pipeline_id,
                 ts,
                 image,
                 vuln.get("Severity"),
